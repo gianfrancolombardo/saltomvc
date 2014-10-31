587f87119ee6eeaa427478345af686e7f855b57a
gianfrancolombardo <gianf.lombardo@gmail.com>
2014-10-31 10:27:12 -0200

    Config package 5

--- main.js
+++ main.js
@@ -1,6 +1,13 @@
+/**
+ * Copyright 2014 Gianfranco Lombardo. All Rights Reserved.
+ *
+ * /main
+ * main.js
+ */
+
 (function () {
     var core = require('./lib/saltomvc');
-//hola
+
     process.argv.forEach(function (val, index, array) {
         if (index >= 2)
             core.index(process.argv[index]);
